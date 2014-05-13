62ce449674fb73f67221445b88517366d935ad01
Manas <super.spa42@gmail.com>
2014-05-13 20:55:21 +0530

    initial DB schema

--- database/schema_ts.sql
+++ database/schema_ts.sql
@@ -1,7 +1,9 @@
---------------------------------
---       Owner & profile
---------------------------------
+/*   ==================================
+       Owner & profile
+================================== */
+
 CREATE TABLE spa (
+  
   spaid int NOT NULL AUTO_INCREMENT,
   spaname varchar(200),
   primaryemail varchar(80) NOT NULL UNIQUE,
@@ -11,6 +13,8 @@ CREATE TABLE spa (
   password varchar(80) ,
   city varchar(80) ,
   area varchar(100) ,
+  spashortname varchar(200), /* Will create URL like www.superspas.in/aromaspa or better yet aromaspa.superspas.in */
+  
   
   ownerfirstname varchar(50),
   ownerlastname varchar(50) ,
@@ -21,17 +25,14 @@ CREATE TABLE spa (
   profilecreatedate date ,
   profilemodifieddate date,
   isHidden int ,
-
   
-  photofilename varchar(100) ,
   shortdescription text,
   longdescription text,
   
   websiteurl text,
   fbprofile text,
-    
-  servicesprovided text,
-  
+  gplusprofile text,
+      
   addressline1 varchar(200),
   addressline2 varchar(200),
   state varchar(100),
@@ -39,15 +40,64 @@ CREATE TABLE spa (
   country varchar(100),
   
   noofvisittoeditprofilepage int,
+  noofuniqueprofilevisits int
   
   PRIMARY KEY (spaid)
 ) AUTO_INCREMENT=1120 
 
 
--------------------------------
---   Tracking table
---   Should be merged with GA
--------------------------------
+----------------------------------------------
+-- This is the table to store the images
+-- for each spa. One spa can have multiple
+-- images associated with them.
+----------------------------------------------
+
+CREATE TABLE images{
+  imageid int NOT NULL AUTO_INCREMENT,
+  spaid int,
+  
+  imageurl text,
+  
+  PRIMARY KEY (imageid),
+  FOREIGN KEY (spaid) REFERENCES spa(spaid)
+}
+
+--------------------------------------------------
+-- This N X N table stores spas and the services 
+-- they provide. For e.g. 
+-- spa1 - Foot Massage
+-- spa1 - Head Massage
+-- spa2 - Foot Massage
+--------------------------------------------------
+CREATE TABLE spa_service{
+	spaserviceid int NOT NULL AUTO_INCREMENT,
+	spaid int,
+	serviceid int,
+	
+    PRIMARY KEY (spaserviceid),
+    FOREIGN KEY (spaid) REFERENCES spa(spaid),
+    FOREIGN KEY (serviceid) REFERENCES spa(serviceid),
+}
+
+------------------------------------------------------
+-- Generic services provided by the spas and salons.
+-- Whenever a spa adds a new service, add it to this 
+-- table first. Then this table is referenced in 
+-- N X N spas_services table
+------------------------------------------------------
+
+CREATE TABLE service{
+   serviceid int NOT NULL AUTO_INCREMENT,
+   servicename varchar(200)
+}
+
+
+------------------------------------------------------
+--   Tracking table for the activities, that happen
+--   Before the customer or spa is loggedon
+--   Explore whether GA has better tracking, of 
+--   "WHO" did it.
+------------------------------------------------------
 CREATE TABLE tracking (
   trackid int NOT NULL AUTO_INCREMENT,
   spaid int ,
